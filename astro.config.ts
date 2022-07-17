a7b1d58ece633fb719f10fc9c00dbc82c2a7311f
kleemeo <leenexer@gmail.com>
2022-07-16 23:55:06 -0400

    update canonicalURL

--- astro.config.ts
+++ astro.config.ts
@@ -3,6 +3,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://kelvinlee.dev/',
   vite: { ssr: { external: ['svgo'] } },
   integrations: [
     tailwind({
